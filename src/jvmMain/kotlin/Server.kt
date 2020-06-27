31ec61b8862b924b7e2bd82d50dd7e4fb550f569
vitek999 <vitek2012rus@gmail.com>
2020-06-27 16:02:20 +0400

    Separate on modules

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,5 +1,7 @@
+import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
@@ -8,11 +10,30 @@ import io.ktor.server.netty.Netty
 
 fun main() {
     val port = 9090
-    embeddedServer(Netty, port) {
-        routing {
-            get("/") {
-                call.respondText("hello from ktor")
-            }
-        }
-    }.start(wait = true)
+    embeddedServer(Netty, port, module = Application::mainModule).start(wait = true)
+}
+
+fun Application.mainModule() {
+    anotherModule()
+    routing {
+        root()
+    }
+}
+
+fun Application.anotherModule() {
+    routing {
+        anotherRouting()
+    }
+}
+
+fun Routing.root() {
+    get("/") {
+        call.respondText("hello from ktor")
+    }
+}
+
+fun Routing.anotherRouting() {
+    get("/another") {
+        call.respondText("hello from another")
+    }
 }
