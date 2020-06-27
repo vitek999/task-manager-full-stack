c6b066d8a75094be40376e790ee65467b968930b
vitek999 <vitek2012rus@gmail.com>
2020-06-27 15:43:58 +0400

    empty ktor server

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,18 @@
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
 fun main() {
-    println("hello from jvm")
+    val port = 9090
+    embeddedServer(Netty, port) {
+        routing {
+            get("/") {
+                call.respondText("hello from ktor")
+            }
+        }
+    }.start(wait = true)
 }
